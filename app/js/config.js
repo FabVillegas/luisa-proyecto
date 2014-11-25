31a07b44285892cbd55ab15df27e9b081d932701
Fabritzio Villegas <f.villegas93@gmail.com>
2014-11-25 00:48:07 -0600

    Creo que esta presentable ahora

--- app/js/config.js
+++ app/js/config.js
@@ -1,31 +1,9 @@
-angular.module('luisa-proyecto', ['ui.router', 'slick', 'firebase', 'angular-carousel']).config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+angular.module('luisa-proyecto', ['ui.router', 'firebase', 'angular-carousel', 'ngDialog']).config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
 	$urlRouterProvider.otherwise('first');
 	$stateProvider.
 	state('first', {
 		url: '/first',
 		templateUrl: 'views/firstTemplate.html',
 		controller: 'firstCtrl'
-	}).
-	state('second', {
-		url: '/second',
-		templateUrl: 'views/secondTemplate.html',
-		controller: 'secondCtrl'
 	});
 }]);
-
-
-var isLoggedIn = function($firebaseSimpleLogin, $state, firebaseRefFactory){
-	var dataRef = new Firebase(firebaseRefFactory.getRef());
-	var loginObj = $firebaseSimpleLogin(dataRef);
-	loginObj.$getCurrentUser().then(function(user){// this returns a promise
-		if(user){
-			console.log('Logged in as:');
-			console.log(loginObj.$getCurrentUser());
-			return;
-		}
-		else{
-			// send user to login state/route
-			$state.go('login');
-		}
-	});
-};
--- app/js/controllers/firstCtrl.js
+++ app/js/controllers/firstCtrl.js
@@ -1,10 +1,36 @@
 angular.module('luisa-proyecto').controller('firstCtrl', firstCtrl);
 
-firstCtrl.$inject = ['$scope', '$state', '$firebase'];
+firstCtrl.$inject = ['$scope', '$state', '$firebase', 'ngDialog'];
 
-function firstCtrl($scope, $state, $firebase){
+function firstCtrl($scope, $state, $firebase, ngDialog){
 
-  $scope.moviesList = $firebase(new Firebase('https://cms-luisa.firebaseio.com/peliculas')).$asArray();
+  $scope.newReleases = $firebase(new Firebase('https://cms-luisa.firebaseio.com/peliculas/New Releases')).$asArray();
+  $scope.popular = $firebase(new Firebase('https://cms-luisa.firebaseio.com/peliculas/Popular')).$asArray();
+  $scope.recentlyAdded = $firebase(new Firebase('https://cms-luisa.firebaseio.com/peliculas/Recently Added')).$asArray();
+  $scope.tvShows = $firebase(new Firebase('https://cms-luisa.firebaseio.com/peliculas/TV Shows')).$asArray();
+
+  $scope.selectedMovie = {};
+  $scope.starsCounter = 0;
+  $scope.numberOfStars = [];
+
+  $scope.getInfo = function(index, category){
+    $scope.numberOfStars = [];
+    var surrogate = $scope.newReleases[index];
+    console.log(surrogate);
+    $scope.selectedMovie = surrogate;
+
+
+    $scope.starsCounter = parseFloat($scope.selectedMovie.rating);
+    for(var i = 1; i <= $scope.starsCounter; i++){
+      $scope.numberOfStars.push({value: i});
+    }
+    ngDialog.open({
+        template: 'movieTemplate',
+        closeByDocument: true,
+        closeByEscape: true,
+        scope: $scope,
+      });
+  };
 
   $scope.printIndex = function(slide){
     console.log(slide);
